6d991c9b9c74d773cf0d656f716f766c26ae9a43
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2023-03-05 12:25:41 +0700

    готовое решение

--- RobotSingPython.py
+++ RobotSingPython.py
@@ -1,16 +1,32 @@
-s = str(input("введите строку для робота: "))
-s = s.replace(" ", "")
-s = s.lower()
-j = 0
-i = 1
-r = 0
+while True:
+    s = str(input("Введите строку для робота: "))
+    s = s.lower()
+    s = s.replace(' ','')    
+    word = 'аоуэяюиыеё'
+    l = list(s)
+    score = 0
+    
+    if s=='end':
+        break
 
-while i <= len(s):
-    if s[i-1] in ('а', 'у', 'о', 'и', 'э', 'ы', 'я', 'ю', 'е', 'ё') or not ('а', 'у', 'о', 'и', 'э', 'ы', 'я', 'ю', 'е', 'ё'):
-        j = 0
-        i = i + 2
-    else:
-        r = r + 1
-        i = i + 2
-
-print(r)
+    if l[0] not in word and l[1] not in word:
+        l.pop(0)
+        l.insert(0,'а')
+        score += 1
+    if l[0] in word and l[1] in word:
+        l.pop(0)
+        l.insert(0,'б')
+        score += 1
+    
+    
+    for j in range(1, len(l)):
+        if l[j] not in word and l[j - 1] not in word:
+            l.pop(j)
+            l.insert(j,'а')
+            score += 1
+        if l[j] in word and l[j - 1] in word:
+            l.pop(j)
+            l.insert(j,'б')
+            score += 1
+    
+    print("Вывод: " + str(score))
